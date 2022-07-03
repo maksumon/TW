e91a57f14b55d9795fdf0308c9b495f8efc272b8
Mohammad Ashraful Kabir <maksumon007@gmail.com>
2022-07-03 16:08:49 +0800

    Initial Commit

---
+++ TW/TWApp.swift
@@ -0,0 +1,17 @@
+//
+//  TWApp.swift
+//  TW
+//
+//  Created by Mohammad Ashraful Kabir on 03/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
